62fe46af221d486d59932ef3be01458cd58e4d23
Steeve Sanon <steevesanon6@gmail.com>
2017-07-20 18:43:59 -0400

    Add Project files

---
+++ app/src/main/java/com/classroom1/gitdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.classroom1.gitdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View view){
+        Toast.makeText(this, "You are now Logged in",Toast.LENGTH_SHORT).show();
+    }
+}
